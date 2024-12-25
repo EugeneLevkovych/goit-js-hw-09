fcd559d99a70e3fa981b01fa13c43cb3d0466692
EugeneLevkovych <eugenelevkovych@yahoo.com>
2024-12-25 23:34:06 +0200

    add if

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,5 +1,5 @@
 const feedbackFormEl = document.querySelector('.feedback-form');
-// console.log(feedbackFormEl);
+
 let formData = {
   email: '',
   message: '',
@@ -25,12 +25,12 @@ const fillFormField = () => {
 
 fillFormField();
 
-const onFormFieldChange = event => {
+const onFormFieldInput = event => {
   // const formFieldEl = event.target;
   const { target: formFieldEl } = event;
   const fieldValue = formFieldEl.value;
   const fieldName = formFieldEl.name;
-
+  console.log(fieldName);
   formData[fieldName] = fieldValue;
 
   localStorage.setItem('feedback-form-state', JSON.stringify(formData));
@@ -38,12 +38,17 @@ const onFormFieldChange = event => {
 
 const onFeedbackFormSubmit = event => {
   event.preventDefault();
+  if (!formData.email || !formData.message) {
+    alert('Fill please all fields');
+    return;
+  }
+
+  console.log(formData);
 
   const { currentTarget: formEl } = event;
-
   formEl.reset();
   localStorage.removeItem('feedback-form-state');
 };
 
-feedbackFormEl.addEventListener('change', onFormFieldChange);
+feedbackFormEl.addEventListener('input', onFormFieldInput);
 feedbackFormEl.addEventListener('submit', onFeedbackFormSubmit);
